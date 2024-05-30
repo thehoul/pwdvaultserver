3291201ac8c347b86acada74ae799b7a384a3491
HOT <theo@houle.pro>
2024-05-30 22:55:21 +0200

    add test for delete password with two domains

--- database/dbmanagertest.py
+++ database/dbmanagertest.py
@@ -1,4 +1,4 @@
-from dbmanager import DbManager
+from database.dbmanager import DbManager
 
 uname = 'admin'
 email = 'admin@localhost'
@@ -48,16 +48,45 @@ def test_delete_password(db):
     res = db.get_password(uname, domain)
     assert not res, f"Expected None, got {res}"
 
+def test_two_domain_delete_password(db):
+    db.register(uname, email, hashpwd, salt)
+
+    pwd1 = 'password1'
+    pwd2 = 'password2'
+    domain1 = 'google.com'
+    domain2 = 'facebook.com'
+
+    db.add_password(uname, domain1, pwd1)
+    db.add_password(uname, domain2, pwd2)
+
+    assert db.get_password(uname, domain1) == pwd1, f"Expected {pwd1}, got {res}"
+    assert db.get_password(uname, domain2) == pwd2, f"Expected {pwd2}, got {res}"
+
+    db.delete_password(uname, domain1)
+
+    res = db.get_password(uname, domain1)
+    assert not res, f"Expected None, got {res}"
+
+    res = db.get_password(uname, domain2)
+    assert res == pwd2, f"Expected {pwd2}, got {res}"
+
+    db.delete_password(uname, domain2)
+
+    res = db.get_password(uname, domain2)
+    assert not res, f"Expected None, got {res}"
+
 # Make main method
 def main():
     db = DbManager()
-    test_register_unregister(db)
-    print("------")
-    test_get_password(db)
-    print("------")
-    test_update_password(db)
-    print("------")
-    test_delete_password(db)
+    #test_register_unregister(db)
+    #print("------")
+    #test_get_password(db)
+    #print("------")
+    #test_update_password(db)
+    #print("------")
+    #test_delete_password(db)
+    #print("------")
+    test_two_domain_delete_password(db)
 
 if __name__ == "__main__":
     main()
