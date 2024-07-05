2d0c77d3aa8b48f88217276612e2a22f01a75459
HOT <theo@houle.pro>
2024-07-05 15:54:14 +0200

    Add verification end points
    Remove https from mainapp
    
    modfiy mailman

--- mailman.py
+++ mailman.py
@@ -1,33 +1,38 @@
 from flask_mail import Mail, Message
 from flask import render_template
-from itsdangerous import URLSafeTimedSerializer
-
-serializer = URLSafeTimedSerializer('secretkey') # TODO: Change this to a random string
-salt = 'superalt' # TODO: Change this to a random string
-
-root = 'http://127.0.0.1:5000'
+from itsdangerous import URLSafeTimedSerializer, exc
 
 mail = Mail()
 
-def generate_token(email):
-    return serializer.dumps(email, salt=salt)
+class MailMan:
 
-def confirm_token(token, expiration=3600):
-    try:
-        email = serializer.loads(
-            token,
-            salt=salt,
-            max_age=expiration
-        )
-    except:
-        return False
-    return email
+    def __init__(self, app):
+        mail.init_app(app)
+        self.salt = app.config['MAIL_SALT']
+        self.serializer = URLSafeTimedSerializer(app.config['MAIL_SECRET_KEY'])
+        self.root = app.config['ROOT_URL']
 
-def send_account_verification_email(username, email):
-    token = generate_token(email)
-    link = f'{root}/verifyAccount?token={token}'
-    html = render_template('mail.html', username=username, link=link)
-    msg = Message('Account Verification', recipients=[email], html=html)
-    mail.send(msg)
+    def generate_token(self, email):
+        return self.serializer.dumps(email, salt=self.salt)
+
+    def confirm_token(self, token, expiration=3600):
+        try:
+            email = self.serializer.loads(
+                token,
+                salt=self.salt,
+                max_age=expiration
+            )
+        except exc.SignatureExpired:
+            return False
+        except exc.BadSignature:
+            return False
+        return email
+
+    def send_account_verification_email(self, username, email):
+        token = self.generate_token(email)
+        link = f'{self.root}/verifyAccount?token={token}'
+        html = render_template('mail.html', username=username, link=link)
+        msg = Message('Account Verification', recipients=[email], html=html)
+        mail.send(msg)
 
 
